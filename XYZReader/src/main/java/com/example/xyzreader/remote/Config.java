e52b25aa2ef844b9e4cbd6633523a226cfb87028
Mykola Dzyuba <mdzyuba@gmail.com>
2019-07-18 23:37:27 -0700

    style: Update style of the article detail fragment
    This change updates properties used in the article
    detail fragment. It also updates the images image
    size in the list of articles. The title bar is
    extracted to article_title_bar.xml layout to make
    code easier to read.

--- XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/Config.java
@@ -9,10 +9,13 @@ public class Config {
     public static final URL BASE_URL;
     private static String TAG = Config.class.toString();
 
+    public static final String READER_JSON = "https://go.udacity.com/xyz-reader-json";
+    public static final String READER_JSON_2 = "https://raw.githubusercontent.com/SuperAwesomeness/XYZReader/master/data.json";
+
     static {
         URL url = null;
         try {
-            url = new URL("https://go.udacity.com/xyz-reader-json" );
+            url = new URL(READER_JSON_2);
         } catch (MalformedURLException ignored) {
             // TODO: throw a real error
             Log.e(TAG, "Please check your internet connection.");
